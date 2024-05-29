4cb7415686130b2b20da771d2447c32d629901b7
rokuro001 <114669748+rokuro001@users.noreply.github.com>
2024-05-29 16:01:47 +0800

    Delete appBantuan directory

--- appBantuan/resources/views/rumah/rumah.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- Menghubungkan dengan view template master -->
-@extends('template.master')
-<!-- isi bagian judul halaman -->
-@section('judul_halaman', 'HALAMAN BANTUAN BEDAH RUMAH')
-<!-- isi bagian konten -->
-@section('konten')
-@endsection
